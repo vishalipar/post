039d6a85eca1546c1625ac8035c6b9fa4f40081c
vishalipar <vishalipar04@gmail.com>
2024-08-10 16:38:41 +0530

    added create-post with delete button

--- main/views.py
+++ main/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,PostForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login,logout,authenticate
+from .models import Post
 # Create your views here.
+
+@login_required(login_url="/login")
 def home(request):
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+    
+    if request.method == 'POST':
+        post_id = request.POST.get("post-id")
+        post = Post.objects.filter(id=post_id).first()
+        if post and post.author == request.user:
+            post.delete()
+        
+    return render(request, 'main/home.html', {'posts':posts})
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+    return render(request,'main/create_post.html', {'form':form})
 
 def sign_up(request):
     if request.method == 'POST':
